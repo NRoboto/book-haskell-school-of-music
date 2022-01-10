c26f7480729454cdcefb7f2e90d45e073bdeb92b
Mike Nightingale <michael.nightingale@candide.com>
2022-01-10 19:17:54 +0000

    Exercise 2.1 + 2.2

---
+++ chapter-2.hs
@@ -0,0 +1,37 @@
+import Euterpea;
+
+t251 :: Music Pitch
+t251 = let dMinor = d 4 wn :=: f 4 wn :=: a 4 wn
+           gMajor = g 4 wn :=: b 4 wn :=: a 4 wn
+           cMajor = c 4 bn :=: e 4 bn :=: g 4 bn
+       in dMinor :+: gMajor :+: cMajor
+
+-- Not entirely correct, trans shifts by a semi-tone, I don't know enough about music to make it shift to not a sharp/flat
+twoFiveOne :: Pitch -> Dur -> Music Pitch
+twoFiveOne pitch dur =
+  let triad :: Pitch -> Dur -> Music Pitch
+      triad p dur' = note dur' p :=: note dur' (trans 3 p) :=: note dur' (trans 3 p)
+  in triad (trans 2 pitch) dur :+: triad (trans 7 pitch) dur :+: triad pitch (dur/2)
+
+data BluesPitchClass = Ro | Mt | Fo | Fi | MS
+type BluesPitch = (BluesPitchClass, Octave)
+ro, mt, fo, fi, ms :: Octave -> Dur -> Music BluesPitch
+ro o d = note d (Ro, o)
+mt o d = note d (Mt, o)
+fo o d = note d (Fo, o)
+fi o d = note d (Fi, o)
+ms o d = note d (MS, o)
+
+fromBlues :: Music BluesPitch -> Music Pitch
+fromBlues (Prim (Note d (bpc, o))) =
+    let bluesToPitchClass :: BluesPitchClass -> PitchClass
+        bluesToPitchClass Ro = C
+        bluesToPitchClass Mt = Ef
+        bluesToPitchClass Fo = F
+        bluesToPitchClass Fi = G
+        bluesToPitchClass MS = Bf
+    in note d (bluesToPitchClass bpc, o)
+fromBlues (Prim (Rest d)) = rest d
+fromBlues (m :+: m') = fromBlues m :+: fromBlues m'
+fromBlues (m :=: m') = fromBlues m :=: fromBlues m'
+fromBlues (Modify c m) = Modify c $ fromBlues m
