937145959ad0049268a46f62b4e5dc59354328f2
Mike Nightingale <michael.nightingale@candide.com>
2022-02-19 17:16:11 +0000

    Exercises 3.7-3.11

--- chapter-3.hs
+++ chapter-3.hs
@@ -61,3 +61,63 @@ applyEach fs a = foldr applyOp [] fs
 -- Exercise 3.5
 applyAll :: [a -> a] -> a -> a
 applyAll = flip $ foldl $ flip ($)
+
+-- Exercise 3.7
+extraParam :: (a -> b) -> (a -> c -> b)
+extraParam f a b = f a
+
+length'' :: [a] -> Integer
+length'' xs = sum $ map (const 1) xs
+
+length''' :: [a] -> Integer
+length''' = foldl (extraParam $ (+) 1) 0
+
+-- Exercise 3.8
+doubleEach :: [Integer] -> [Integer]
+doubleEach = map (* 2)
+
+type IntegerPair = (Integer, Integer)
+
+pairAndOne :: [Integer] -> [IntegerPair]
+pairAndOne = map toPairAndOne
+             where toPairAndOne x = (x, x + 1)
+
+addEachPair :: [IntegerPair] -> [Integer]
+addEachPair = map sumPair
+              where sumPair (x, y) = x + y
+
+addPairsPointwise :: [IntegerPair] -> IntegerPair
+addPairsPointwise = foldr addPointwise (0, 0)
+                    where addPointwise (x1, x2) (y1, y2) = (x1 + y1, x2 + y2)
+
+-- Exercise 3.9
+fuse :: [Dur] -> [Dur -> Music a] -> [Music a]
+fuse [] [] = []
+fuse _ [] = error "fuse: Lists have unequal length"
+fuse [] _ = error "fuse: Lists have unequal length"
+fuse (d:ds) (f:fs) = f d : fuse ds fs
+
+-- Exercise 3.10
+maxAbsPitch :: [AbsPitch] -> AbsPitch
+maxAbsPitch [] = error "maxAbsPitch: Empty list"
+maxAbsPitch xs = mAbs 0 xs
+              where mAbs curr (x:xs) = if x > curr then mAbs x xs else mAbs curr xs
+                    mAbs curr [] = curr
+
+maxAbsPitch' :: [AbsPitch] -> AbsPitch
+maxAbsPitch' = foldr1 greaterPitch
+               where greaterPitch x y = if x > y then x else y
+
+maxAbsPitch'' :: [AbsPitch] -> AbsPitch
+maxAbsPitch'' = foldr1 max
+
+minAbsPitch'' :: [AbsPitch] -> AbsPitch
+minAbsPitch'' = foldr1 min
+
+-- Exercise 3.11
+-- |Chromatic scale between pitches p1 and p2, notes are separated by one semitone (one absolute pitch)
+chrom :: Pitch -> Pitch -> Music Pitch 
+chrom p1 p2 = foldr ((:+:) . note qn . pitch) (rest 0) absPitchRange
+              where absPitches = (absPitch p1, absPitch p2)
+                    rangeDirection = if uncurry (>) absPitches then -1 else if uncurry (==) absPitches then 0 else 1
+                    absPitchRange = [fst absPitches, fst absPitches + rangeDirection..snd absPitches]
