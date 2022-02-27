9fb1ae32ce0a65a3f853398ac5bf5b7f63dceb69
Mike Nightingale <michael.nightingale@candide.com>
2022-02-27 11:10:31 +0000

    Exercise 6.3

--- chapter-6.hs
+++ chapter-6.hs
@@ -15,11 +15,23 @@ invert' m = line $ map inv l
                   inv (Prim (Rest d)) = rest d
 
 -- Exercise 6.2
+isNote :: Music a -> Bool
+isNote (Prim (Note _ _)) = True 
+isNote (Prim (Rest _)) = False
+
+musicToPitchClass :: Music (a, b) -> a
+musicToPitchClass (Prim (Note _ (pc, _))) = pc
+
 -- |Determines whether a line of music has exactly twelve notes, and each pitch class is unique
 properRow :: Music Pitch -> Bool
 properRow m = length (uniquePitchClasses $ map musicToPitchClass $ filter isNote $ lineToList m) == 12
-            where isNote (Prim (Note _ _)) = True 
-                  isNote (Prim (Rest _)) = False
-                  musicToPitchClass (Prim (Note _ (pc, _))) = pc
-                  uniquePitchClasses :: [PitchClass] -> [PitchClass]
+            where uniquePitchClasses :: [PitchClass] -> [PitchClass]
                   uniquePitchClasses xs = foldr (\pc pcs -> if pc `elem` pcs then pcs else pc:pcs) [] xs
+
+-- Exercise 6.3
+-- |Determines whether a line of music is a palindrome
+palin :: Music Pitch -> Bool
+palin = isPalindrome . map musicToPitchClass . filter isNote . lineToList
+      where isPalindrome [] = True
+            isPalindrome [_] = True
+            isPalindrome l@(x:xs) = (x == last l) && isPalindrome (init xs)
