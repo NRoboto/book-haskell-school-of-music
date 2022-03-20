99640016de5f1a9ea1d8823491b28fe5307014c9
Mike Nightingale <michael.nightingale@candide.com>
2022-03-20 11:03:25 +0000

    Chapter 8

---
+++ chapter-8.hs
@@ -0,0 +1,11 @@
+data MyData = Data1 {
+  myFloat :: Float,
+  myInteger :: Integer,
+  myBool :: Bool}
+
+dataInst = Data1 {
+  myFloat = 3.2,
+  myInteger = 5,
+  myBool = True}
+
+outputMyFloat = print $ myFloat dataInst
